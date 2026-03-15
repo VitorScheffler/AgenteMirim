60b9ea4ea40048872e776b4781300e3659a9ca2d
vitor <vitor.scheffler@hotmail.com>
2026-03-15 20:11:35 -0300

    Initial commit

--- app/src/main/java/br/feevale/agentemirim/MainActivity.java
+++ app/src/main/java/br/feevale/agentemirim/MainActivity.java
@@ -2,19 +2,36 @@ package br.feevale.agentemirim;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSair = findViewById(R.id.btnSair);
-        btnSair.setOnClickListener(v -> {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView ivUsuario = findViewById(R.id.ivUsuario);
+        ivUsuario.setOnClickListener(view -> {
+            PopupMenu popup = new PopupMenu(this, view);
+            popup.getMenuInflater().inflate(R.menu.menu_usuario, popup.getMenu());
+
+            popup.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.action_sair) {
+                    startActivity(new Intent(this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            });
+
+            popup.show();
         });
     }
 }
